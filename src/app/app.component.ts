df6faaf459d51b214ee1611f174e1fa7e80e7dbc
Adinael Perez Ruelas <adin692@gmail.com>
2019-06-19 19:28:03 -0600

    commit for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
-import {Component, Inject} from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
+import 'babel-polyfill';
+import { Component, Inject } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import TransportU2F from '@ledgerhq/hw-transport-u2f';
+import Btc from '@ledgerhq/hw-app-btc';
+
+const getDevice = async () => {
+  const transport = await TransportU2F.create();
+  const ledger = new Btc(transport);
+
+  ledger.close = () => transport.close();
+
+  return ledger;
+};
 
 @Component({
   selector: 'app-root',
@@ -10,19 +22,13 @@ export class AppComponent {
   title: string;
 
   constructor(private translate: TranslateService, @Inject('state') private state) {
-    // all languages used
-    const langs = ['en', 'de'];
-    const browserLang = translate.getBrowserLang();
-    translate.addLangs(langs);
-    // setting the default lang - if no lang is used
-    translate.setDefaultLang('en');
-    // setting the language depending on the browser setting
-    if (langs.indexOf(browserLang) > -1) {
-      translate.use(browserLang);
-    }
+    this.tryLedger();
 
-    // just for testing
-    // translate.use('de');
+  }
 
+  private async tryLedger() {
+    const x = await getDevice();
+
+    console.log(x);
   }
 }
